22a79625926de8b1cc74e77b5dc5b5085210871e
unknown <davidjhovsepian@gmail.com>
2024-02-16 12:01:10 +0100

    here it is baby

---
+++ custom_addons/davids_report_test_thing/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "David's Reports",
+    'summary': "A module for generating reports by David Hovsepian",
+    'version': '1.0',
+    'author': 'David Hovsepian',
+    'category': 'Customization/Reporting',
+
+    'data': [
+        'views/snippets/s_image_text.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
